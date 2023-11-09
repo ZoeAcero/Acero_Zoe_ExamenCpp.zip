592d687c9ff6d49e3f25144bf42a2fce74c2a731
ZoeAcero <zar.amazona@gmail.com>
2023-11-09 11:37:04 +0100

    guardado EJ1

---
+++ parte 2 EJ1/Operaciones basicas.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main() {
+    int a, b, suma = 0;
+
+    cout << "Ingrese el primer numero: ";
+    cin >> a;
+
+    cout << "Ingrese el segundo numero: ";
+    cin >> b;
+
+    suma = a + b;
+
+    cout << "La suma de los numeros es: " << suma << endl;
+
+
+    return 0;
+}
